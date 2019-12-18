8aa9a27a6605f5a0fd6ecfb0cca2a4c9387301ad
Mimi <mimitraugott@gmail.com>
2019-12-18 12:34:46 -0700

    first push

--- __instructor-code__/src/App.js
+++ __instructor-code__/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Header from './Components/Header';
-import routes from './routes'
+import routes from './routes';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       {routes}
     </div>
   );
